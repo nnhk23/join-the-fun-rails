53d8be512330a556a0dd32cd35b3bf48e891f726
Kim Nguyen <nnhk23@gmail.com>
2020-10-29 10:36:52 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,25 @@
 class Ride < ActiveRecord::Base
+    belongs_to :passenger
+    belongs_to :taxi
+
+    # def self.add_passengers
+    #     self.all.each do |ride|
+    #         Passenger.all.each do |p|
+    #             if ride.passenger_id == p.id
+    #                 ride.passenger = p
+    #             end
+    #         end
+    #     end
+    # end
+
+    # def self.add_taxi
+    #     self.all.each do |ride|
+    #         Taxi.all.each do |t|
+    #             if ride.taxi_id == t.id
+    #                 ride.taxi = t
+    #             end
+    #         end
+    #     end
+    # end
+
 end
